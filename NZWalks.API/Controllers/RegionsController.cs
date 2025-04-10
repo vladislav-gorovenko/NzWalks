32add5b87332db9e4e9d0e1dcff2ce35263de651
vladislav-gorovenko <vladislav.gorovenko.webdev@gmail.com>
2025-04-10 20:02:22 +0400

    feat/section4: add repository pattern automapper and async await

--- NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using NZWalks.API.Data;
 using NZWalks.API.Models.Domain;
 using NZWalks.API.Models.DTOs;
@@ -18,10 +19,10 @@ public class RegionsController : ControllerBase
 
     // GET ALL REGIONS
     [HttpGet]
-    public IActionResult GetAll()
+    public async Task<IActionResult> GetAll()
     {
         // 1. Get Data From Database - Domain models 
-        var regionsDomain = _dbContext.Regions.ToList();
+        var regionsDomain = await _dbContext.Regions.ToArrayAsync();
 
         // 2. Convert Data to DTOs 
         var regionsDto = new List<RegionDto>();
@@ -44,10 +45,10 @@ public class RegionsController : ControllerBase
     // GET SINGLE REGION BY ID
     [HttpGet]
     [Route("{id:Guid}")]
-    public IActionResult GetById([FromRoute] Guid id)
+    public async Task<IActionResult> GetById([FromRoute] Guid id)
     {
         // 1. Get Data From Database - Domain models 
-        var regionDomain = _dbContext.Regions.FirstOrDefault(x => x.Id == id);
+        var regionDomain = await _dbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
         if (regionDomain == null) return NotFound();
 
         // 2. Convert Data to DTOs 
@@ -65,7 +66,7 @@ public class RegionsController : ControllerBase
 
     // CREATE NEW REGION
     [HttpPost]
-    public IActionResult Create([FromBody] AddRegionRequestDto addRegionRequestDto)
+    public async Task<IActionResult> Create([FromBody] AddRegionRequestDto addRegionRequestDto)
     {
         // 1. Map or Convert DTO to Domain Model
         var regionDomain = new Region()
@@ -76,8 +77,8 @@ public class RegionsController : ControllerBase
         };
 
         // 2. Adding new entity to db 
-        _dbContext.Regions.Add(regionDomain);
-        _dbContext.SaveChanges();
+        await  _dbContext.Regions.AddAsync(regionDomain);
+        await _dbContext.SaveChangesAsync();
 
         // 3. Map Domain Model back to DTO 
         var regionDto = new RegionDto()
@@ -93,10 +94,10 @@ public class RegionsController : ControllerBase
     // UPDATE REGION
     [HttpPut]
     [Route("{id:Guid}")]
-    public IActionResult Update([FromBody] UpdateRegionRequestDto updateRegionRequestDto, [FromRoute] Guid id)
+    public async Task<IActionResult> Update([FromBody] UpdateRegionRequestDto updateRegionRequestDto, [FromRoute] Guid id)
     {
         // 1. Find Domain Model
-        var regionDomain = _dbContext.Regions.FirstOrDefault(x => x.Id == id);
+        var regionDomain = await _dbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
         if (regionDomain == null) return NotFound();
 
         // 2. Update Domain Model Values
@@ -105,8 +106,7 @@ public class RegionsController : ControllerBase
         regionDomain.Code = updateRegionRequestDto.Code;
 
         // 3. Update Db 
-        _dbContext.Update(regionDomain);
-        _dbContext.SaveChanges();
+        await _dbContext.SaveChangesAsync();
 
         // 4. Map Domain Model back to DTO 
         var regionDto = new RegionDto()
@@ -123,15 +123,15 @@ public class RegionsController : ControllerBase
     // DELETE REGION
     [HttpDelete]
     [Route("{id:Guid}")]
-    public IActionResult Delete([FromRoute] Guid id)
+    public async Task<IActionResult> Delete([FromRoute] Guid id)
     {
         // 1. Find domain model region
-        var regionDomain = _dbContext.Regions.FirstOrDefault(x => x.Id == id);
+        var regionDomain = await _dbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
         if (regionDomain == null) return NotFound();
         
         // 2. Remove it from database
         _dbContext.Regions.Remove(regionDomain);
-        _dbContext.SaveChanges();
+        await _dbContext.SaveChangesAsync();
         
         // 3. Return no content
         return NoContent();
